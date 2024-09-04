35f0492047ec934a0acf08e5f700d8044d5b20b7
Matej Gorjanc <matej.gorjanc@salesqueze.com>
2024-09-04 10:12:00 +0200

    Update publish script.

--- publish.sh
+++ publish.sh
@@ -6,12 +6,9 @@ yarn run test
 yarn run build:ts
 yarn run build:ts:es2015
 
-JSCODE="
-var x = require('./package.json');
-x.version = x.version.split('.').map((x, i) => + (i === 2) + + x).join('.');
-fs.writeFileSync('./package.json', JSON.stringify(x, null, 2));
-"
-node -e "$JSCODE"
+node -e "var x = require('./package.json'); \
+         x.version = x.version.split('.').map((x, i) => + (i === 2) + + x).join('.'); \
+         fs.writeFileSync('./package.json', JSON.stringify(x, null, 2));"
 
 rm -rf ./packet/*
 mkdir -p ./packet/lib
@@ -25,14 +22,11 @@ echo "Publishing es6"
 yarn publish --non-interactive --access public
 echo "Publishing es6 ... done"
 
-JSCODE="
-var np = require('./package.json');
-var op = require('./../package.json');
-op.version = np.version;
-var fs = require('fs');
-fs.writeFileSync('./../package.json', JSON.stringify(op, null, 2));
-"
-node -e "$JSCODE"
+node -e "var np = require('./package.json'); \
+         var op = require('./../package.json'); \
+         op.version = np.version; \
+         var fs = require('fs'); \
+         fs.writeFileSync('./../package.json', JSON.stringify(op, null, 2));"
 echo "Version bump ... done"
 
 # es2015
@@ -41,13 +35,10 @@ rm -rf ./packet/lib/*
 cp -r ./lib5/* ./packet/lib/
 cd ./packet
 
-JSCODE="
-var p = require('./package.json');
-p.name = '@kalmia/jsf-common-es2015'
-var fs = require('fs');
-fs.writeFileSync('./package.json', JSON.stringify(p));
-"
-node -e "$JSCODE"
+node -e "var p = require('./package.json'); \
+         p.name = '@kalmia/jsf-common-es2015'; \
+         var fs = require('fs'); \
+         fs.writeFileSync('./package.json', JSON.stringify(p));"
 
 echo "Publishing es2015"
 yarn publish --non-interactive --access public
